2118147572727b4dd96030461cef514c14ae3bec
Bob Steers <kargathia@hotmail.com>
2018-03-16 13:04:33 +0100

    implement and test message coercion

--- brewblox_devcon_spark/serial_comm.py
+++ brewblox_devcon_spark/serial_comm.py
@@ -10,7 +10,6 @@ from typing import Tuple, Iterable, Generator, Callable
 import serial
 from serial.aio import SerialTransport
 from serial.tools import list_ports
-from overrides import overrides
 
 LOGGER = logging.getLogger(__name__)
 
@@ -32,114 +31,60 @@ AUTO_PORT = 'auto'
 
 
 class SerialProtocol(asyncio.Protocol):
-    def __init__(self):
+    def __init__(self, loop):
         super().__init__()
-        self._msg_queue = asyncio.Queue()
+        self._loop = loop
+        self._data_messages = asyncio.Queue()
+        self._event_messages = asyncio.Queue()
         self._buffer = ''
-        self._transport = None
 
-    def __str__(self):
-        return f'<{type(self).__name__}>'
+    @property
+    def events(self):
+        return self._event_messages
 
-    @overrides
+    @property
+    def data(self):
+        return self._data_messages
+
     def connection_made(self, transport):
-        self._transport = transport
-        self._transport.serial.rts = False
+        transport.serial.rts = False
         LOGGER.debug(f'Serial connection made: {transport}')
 
-    @overrides
     def data_received(self, data):
         self._buffer += data.decode()
 
-        # '<add>0A<id>00<OneWir<!connected:sensor>eTempSensor>01<address>28C80E9A0300009C\n'
+        for event in self._coerce_message_from_buffer(start='<', end='>', filter_expr='!([\s\S]*)'):
+            self._event_messages.put_nowait(event)
 
-        # for event in self._parse(start='<', end='>'):
-        #     pass
+        for data in self._coerce_message_from_buffer(start='^', end='\n'):
+            self._data_messages.put_nowait(data)
 
-        # for data in self._parse(start='^', end='\n'):
-        #     pass
-
-        # for event in self._parse_events_from_buffer():
-        #     '<add>'
-        #     '<id>'
-        #     '<!connected:sensor>'  # yielded
-        #     '<OnewireTempSensor>'
-        #     '<address>'
-
-        # leftovers 0a000128C80E9A0300009C\n
-
-        # for data in self._parse_data_from_buffer():
-        #     '0a000128C80E9A0300009C'  # yield
-
-        # for anno in self._coerce_annotations_from_buffer():
-        #     if anno[0] in '![':
-        #         self.on_annotations(anno)
-
-        # for data in self._coerce_data_from_buffer():
-        #     self.on_data(data)
-
-        # coerce debug from buffer
-        # coerce data from buffer
-
-        # for line in self._coerce_message_from_buffer():
-        #     LOGGER.debug(f'new full line: {line}')
-        #     line = line.replace(' ', '')
-        #     self._msg_queue.put_nowait(line.encode())  # FIXME: Make sure it's right
-
-    @overrides
     def connection_lost(self, exc):
-        LOGGER.debug(f'port closed: {exc}')
-        asyncio.get_event_loop().stop()
-
-    # async def watch_messages(self):
-    #     LOGGER.debug('watching message queue...')
-    #     while True:
-    #         yield await self._msg_queue.get()
+        LOGGER.warn(f'port closed: {exc}')
 
     def _coerce_message_from_buffer(self, start: str, end: str, filter_expr: str=None):
         messages = []
 
         def remove_message(matchobj) -> str:
-            msg = matchobj.group()
-            LOGGER.info(msg)
-            # discard all messages that do not match the (optional) filter expression
-            if filter_expr is None or re.match(filter_expr, msg) is not None:
+            msg = matchobj.group('message')
+
+            if filter_expr is None:
                 messages.append(msg)
+            else:
+                [messages.append(m) for m in re.findall(filter_expr, msg)]
+
             return ''
 
-        while end in self._buffer:
+        while re.search(f'.*{start}.*{end}', self._buffer):
             self._buffer = re.sub(
-                pattern=f'{start}.*?{end}',
+                pattern=f'{start}(?P<message>[^{start}]*?){end}',
                 repl=remove_message,
                 string=self._buffer,
                 count=1)
 
         yield from messages
 
-    def _orig_coerce_message_from_buffer(self):
-        """
-        Try to make a message out of the buffer and find log messages intertwined
-        into the buffer.
-        """
-        log_messages = []
-        while '\n' in self._buffer:
-            # stripped_buffer, log_messages = self._filter_out_log_messages(self.buffer)
-            if len(log_messages) > 0:
-                yield from log_messages
-                # self._buffer = stripped_buffer
-                continue
 
-            lines = self._buffer.partition('\n')  # returns 3-tuple with line, separator, rest
-            if not lines[1]:
-                # '\n' not found, first element is incomplete line
-                self._buffer = lines[0]
-            else:
-                # complete line received, [0] is complete line [1] is separator [2] is the rest
-                self._buffer = lines[2]
-                yield lines[0].rstrip('\r').rstrip('\n')
-
-
-# Keep??
 class SerialConduit():
 
     def __init__(self, port: str=AUTO_PORT, loop: asyncio.BaseEventLoop=None):
@@ -147,7 +92,7 @@ class SerialConduit():
         self.port = detect_port(port)
         self.transport = None
         self.serial = serial.serial_for_url(port, baudrate=DEFAULT_BAUD_RATE)
-        self.protocol = SerialProtocol()
+        self.protocol = SerialProtocol(self._loop)
 
     def bind(self):
         self.transport = SerialTransport(self._loop, self.protocol, self.serial)
@@ -168,12 +113,12 @@ def all_serial_devices() -> Iterable[DeviceType_]:
     return tuple(list_ports.comports())
 
 
-def all_serial_ports() -> Generator[str]:
+def all_serial_ports() -> Generator[str, None, None]:
     for (port, name, desc) in all_serial_devices():
         yield port
 
 
-def recognized_serial_ports(devices: Iterable[DeviceType_]) -> Generator[str]:
+def recognized_serial_ports(devices: Iterable[DeviceType_]) -> Generator[str, None, None]:
     devices = devices if devices is not None else all_serial_devices()
     for device in devices:
         if is_recognized_device(device):
---
+++ test/test_serial_comm.py
@@ -0,0 +1,72 @@
+"""
+Tests brewblox_devcon_spark.serial_comm
+"""
+
+import pytest
+from brewblox_devcon_spark import serial_comm
+
+TESTED = serial_comm.__name__
+
+
+@pytest.fixture
+def serial_data():
+    chunks = [
+        '<add>0A<id>00<OneWir<!connected:sen'.encode(),
+        'sor>eTem<!n'.encode(),
+        'ewline\nmessage>pSensor>01<address>28C80E'.encode(),
+        '9A0300009C\n'.encode(),
+        '34234<!connected:mess<!interrupt>'.encode(),
+        'age>\n'.encode()
+    ]
+
+    return chunks
+
+
+async def test_coerce_events(loop, serial_data):
+    p = serial_comm.SerialProtocol(loop)
+    [p.data_received(chunk) for chunk in serial_data]
+
+    expected_items = [
+        'connected:sensor',
+        'newline\nmessage',
+        'interrupt',
+        'connected:message',
+    ]
+    assert len(expected_items) == p.events.qsize()
+    for expected in expected_items:
+        actual = p.events.get_nowait()
+        assert actual == expected
+
+
+async def test_coerce_data(loop, serial_data):
+    p = serial_comm.SerialProtocol(loop)
+    [p.data_received(chunk) for chunk in serial_data]
+
+    expected_items = [
+        '0A''00''01''28C80E9A0300009C',
+        '34234'
+    ]
+    assert len(expected_items) == p.data.qsize()
+    for expected in expected_items:
+        actual = p.data.get_nowait()
+        assert actual == expected
+
+
+async def test_coerce_partial(loop, serial_data):
+    p = serial_comm.SerialProtocol(loop)
+
+    p.data_received(serial_data[0])
+    assert p.events.empty()
+    assert p.data.empty()
+
+    p.data_received(serial_data[1])
+    assert p.events.get_nowait() == 'connected:sensor'
+    assert p.data.empty()
+
+    p.data_received(serial_data[2])
+    p.data_received(serial_data[3])
+    assert p.events.get_nowait() == 'newline\nmessage'
+    assert p.data.get_nowait() == '0A''00''01''28C80E9A0300009C'
+
+    assert p.events.empty()
+    assert p.data.empty()
