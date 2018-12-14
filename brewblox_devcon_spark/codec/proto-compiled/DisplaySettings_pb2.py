0afbbb0dd6c418d5f16bc7bde751fac2b2b32b07
Elco Jacobs <elco@brewpi.com>
2018-12-14 23:19:26 +0100

    Update display proto

--- brewblox_devcon_spark/codec/proto-compiled/DisplaySettings_pb2.py
+++ brewblox_devcon_spark/codec/proto-compiled/DisplaySettings_pb2.py
@@ -21,43 +21,43 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='blox',
   syntax='proto3',
   serialized_options=None,
-  serialized_pb=_b('\n\x15\x44isplaySettings.proto\x12\x04\x62lox\x1a\x0e\x62rewblox.proto\x1a\x0cnanopb.proto\"\xde\x01\n\x0f\x44isplaySettings\x1a\xc2\x01\n\x06Screen\x12;\n\x07widgets\x18\x01 \x03(\x0b\x32#.blox.DisplaySettings.Screen.WidgetB\x05\x92?\x02\x10\x06\x1a{\n\x06Widget\x12\x12\n\x03pos\x18\x01 \x01(\rB\x05\x92?\x02\x38\x08\x12\x1c\n\x08\x62g_color\x18\x02 \x01(\x0c\x42\n\x92?\x02\x08\x03\x92?\x02x\x01\x12\x13\n\x04name\x18\x03 \x01(\tB\x05\x92?\x02\x08\x10\x12#\n\x0cProcessValue\x18\n \x01(\rB\x0b\x8a\xb5\x18\x02\x18\x01\x92?\x02\x38\x10H\x00\x42\x05\n\x03obj:\x06\x92?\x03H\xba\x02\x62\x06proto3')
+  serialized_pb=_b('\n\x15\x44isplaySettings.proto\x12\x04\x62lox\x1a\x0e\x62rewblox.proto\x1a\x0cnanopb.proto\"\xe1\x01\n\x0f\x44isplaySettings\x12\x34\n\x07widgets\x18\x01 \x03(\x0b\x32\x1c.blox.DisplaySettings.WidgetB\x05\x92?\x02\x10\x06\x12\x13\n\x04name\x18\x02 \x01(\tB\x05\x92?\x02\x08 \x1a{\n\x06Widget\x12\x12\n\x03pos\x18\x01 \x01(\rB\x05\x92?\x02\x38\x08\x12\x1c\n\x08\x62g_color\x18\x02 \x01(\x0c\x42\n\x92?\x02\x08\x03\x92?\x02x\x01\x12\x13\n\x04name\x18\x03 \x01(\tB\x05\x92?\x02\x08\x10\x12#\n\x0cProcessValue\x18\n \x01(\rB\x0b\x8a\xb5\x18\x02\x18\x01\x92?\x02\x38\x10H\x00\x42\x05\n\x03obj:\x06\x92?\x03H\xba\x02\x62\x06proto3')
   ,
   dependencies=[brewblox__pb2.DESCRIPTOR,nanopb__pb2.DESCRIPTOR,])
 
 
 
 
-_DISPLAYSETTINGS_SCREEN_WIDGET = _descriptor.Descriptor(
+_DISPLAYSETTINGS_WIDGET = _descriptor.Descriptor(
   name='Widget',
-  full_name='blox.DisplaySettings.Screen.Widget',
+  full_name='blox.DisplaySettings.Widget',
   filename=None,
   file=DESCRIPTOR,
   containing_type=None,
   fields=[
     _descriptor.FieldDescriptor(
-      name='pos', full_name='blox.DisplaySettings.Screen.Widget.pos', index=0,
+      name='pos', full_name='blox.DisplaySettings.Widget.pos', index=0,
       number=1, type=13, cpp_type=3, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=_b('\222?\0028\010'), file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='bg_color', full_name='blox.DisplaySettings.Screen.Widget.bg_color', index=1,
+      name='bg_color', full_name='blox.DisplaySettings.Widget.bg_color', index=1,
       number=2, type=12, cpp_type=9, label=1,
       has_default_value=False, default_value=_b(""),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=_b('\222?\002\010\003\222?\002x\001'), file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='name', full_name='blox.DisplaySettings.Screen.Widget.name', index=2,
+      name='name', full_name='blox.DisplaySettings.Widget.name', index=2,
       number=3, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=_b("").decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=_b('\222?\002\010\020'), file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='ProcessValue', full_name='blox.DisplaySettings.Screen.Widget.ProcessValue', index=3,
+      name='ProcessValue', full_name='blox.DisplaySettings.Widget.ProcessValue', index=3,
       number=10, type=13, cpp_type=3, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
@@ -75,41 +75,11 @@ _DISPLAYSETTINGS_SCREEN_WIDGET = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
     _descriptor.OneofDescriptor(
-      name='obj', full_name='blox.DisplaySettings.Screen.Widget.obj',
+      name='obj', full_name='blox.DisplaySettings.Widget.obj',
       index=0, containing_type=None, fields=[]),
   ],
-  serialized_start=153,
-  serialized_end=276,
-)
-
-_DISPLAYSETTINGS_SCREEN = _descriptor.Descriptor(
-  name='Screen',
-  full_name='blox.DisplaySettings.Screen',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  fields=[
-    _descriptor.FieldDescriptor(
-      name='widgets', full_name='blox.DisplaySettings.Screen.widgets', index=0,
-      number=1, type=11, cpp_type=10, label=3,
-      has_default_value=False, default_value=[],
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=_b('\222?\002\020\006'), file=DESCRIPTOR),
-  ],
-  extensions=[
-  ],
-  nested_types=[_DISPLAYSETTINGS_SCREEN_WIDGET, ],
-  enum_types=[
-  ],
-  serialized_options=None,
-  is_extendable=False,
-  syntax='proto3',
-  extension_ranges=[],
-  oneofs=[
-  ],
-  serialized_start=82,
-  serialized_end=276,
+  serialized_start=156,
+  serialized_end=279,
 )
 
 _DISPLAYSETTINGS = _descriptor.Descriptor(
@@ -119,10 +89,24 @@ _DISPLAYSETTINGS = _descriptor.Descriptor(
   file=DESCRIPTOR,
   containing_type=None,
   fields=[
+    _descriptor.FieldDescriptor(
+      name='widgets', full_name='blox.DisplaySettings.widgets', index=0,
+      number=1, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=_b('\222?\002\020\006'), file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='name', full_name='blox.DisplaySettings.name', index=1,
+      number=2, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=_b('\222?\002\010 '), file=DESCRIPTOR),
   ],
   extensions=[
   ],
-  nested_types=[_DISPLAYSETTINGS_SCREEN, ],
+  nested_types=[_DISPLAYSETTINGS_WIDGET, ],
   enum_types=[
   ],
   serialized_options=_b('\222?\003H\272\002'),
@@ -132,31 +116,23 @@ _DISPLAYSETTINGS = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=62,
-  serialized_end=284,
+  serialized_end=287,
 )
 
-_DISPLAYSETTINGS_SCREEN_WIDGET.containing_type = _DISPLAYSETTINGS_SCREEN
-_DISPLAYSETTINGS_SCREEN_WIDGET.oneofs_by_name['obj'].fields.append(
-  _DISPLAYSETTINGS_SCREEN_WIDGET.fields_by_name['ProcessValue'])
-_DISPLAYSETTINGS_SCREEN_WIDGET.fields_by_name['ProcessValue'].containing_oneof = _DISPLAYSETTINGS_SCREEN_WIDGET.oneofs_by_name['obj']
-_DISPLAYSETTINGS_SCREEN.fields_by_name['widgets'].message_type = _DISPLAYSETTINGS_SCREEN_WIDGET
-_DISPLAYSETTINGS_SCREEN.containing_type = _DISPLAYSETTINGS
+_DISPLAYSETTINGS_WIDGET.containing_type = _DISPLAYSETTINGS
+_DISPLAYSETTINGS_WIDGET.oneofs_by_name['obj'].fields.append(
+  _DISPLAYSETTINGS_WIDGET.fields_by_name['ProcessValue'])
+_DISPLAYSETTINGS_WIDGET.fields_by_name['ProcessValue'].containing_oneof = _DISPLAYSETTINGS_WIDGET.oneofs_by_name['obj']
+_DISPLAYSETTINGS.fields_by_name['widgets'].message_type = _DISPLAYSETTINGS_WIDGET
 DESCRIPTOR.message_types_by_name['DisplaySettings'] = _DISPLAYSETTINGS
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 DisplaySettings = _reflection.GeneratedProtocolMessageType('DisplaySettings', (_message.Message,), dict(
 
-  Screen = _reflection.GeneratedProtocolMessageType('Screen', (_message.Message,), dict(
-
-    Widget = _reflection.GeneratedProtocolMessageType('Widget', (_message.Message,), dict(
-      DESCRIPTOR = _DISPLAYSETTINGS_SCREEN_WIDGET,
-      __module__ = 'DisplaySettings_pb2'
-      # @@protoc_insertion_point(class_scope:blox.DisplaySettings.Screen.Widget)
-      ))
-    ,
-    DESCRIPTOR = _DISPLAYSETTINGS_SCREEN,
+  Widget = _reflection.GeneratedProtocolMessageType('Widget', (_message.Message,), dict(
+    DESCRIPTOR = _DISPLAYSETTINGS_WIDGET,
     __module__ = 'DisplaySettings_pb2'
-    # @@protoc_insertion_point(class_scope:blox.DisplaySettings.Screen)
+    # @@protoc_insertion_point(class_scope:blox.DisplaySettings.Widget)
     ))
   ,
   DESCRIPTOR = _DISPLAYSETTINGS,
@@ -164,14 +140,14 @@ DisplaySettings = _reflection.GeneratedProtocolMessageType('DisplaySettings', (_
   # @@protoc_insertion_point(class_scope:blox.DisplaySettings)
   ))
 _sym_db.RegisterMessage(DisplaySettings)
-_sym_db.RegisterMessage(DisplaySettings.Screen)
-_sym_db.RegisterMessage(DisplaySettings.Screen.Widget)
+_sym_db.RegisterMessage(DisplaySettings.Widget)
 
 
-_DISPLAYSETTINGS_SCREEN_WIDGET.fields_by_name['pos']._options = None
-_DISPLAYSETTINGS_SCREEN_WIDGET.fields_by_name['bg_color']._options = None
-_DISPLAYSETTINGS_SCREEN_WIDGET.fields_by_name['name']._options = None
-_DISPLAYSETTINGS_SCREEN_WIDGET.fields_by_name['ProcessValue']._options = None
-_DISPLAYSETTINGS_SCREEN.fields_by_name['widgets']._options = None
+_DISPLAYSETTINGS_WIDGET.fields_by_name['pos']._options = None
+_DISPLAYSETTINGS_WIDGET.fields_by_name['bg_color']._options = None
+_DISPLAYSETTINGS_WIDGET.fields_by_name['name']._options = None
+_DISPLAYSETTINGS_WIDGET.fields_by_name['ProcessValue']._options = None
+_DISPLAYSETTINGS.fields_by_name['widgets']._options = None
+_DISPLAYSETTINGS.fields_by_name['name']._options = None
 _DISPLAYSETTINGS._options = None
 # @@protoc_insertion_point(module_scope)
