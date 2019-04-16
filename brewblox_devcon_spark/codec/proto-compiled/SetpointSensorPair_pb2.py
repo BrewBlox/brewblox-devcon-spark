8e70d7bc1c80759ee576c372c3c814bf477b9e01
Elco Jacobs <elco@brewpi.com>
2019-04-16 16:28:30 +0200

    proto update for separate stored setting in sspair

--- brewblox_devcon_spark/codec/proto-compiled/SetpointSensorPair_pb2.py
+++ brewblox_devcon_spark/codec/proto-compiled/SetpointSensorPair_pb2.py
@@ -21,7 +21,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='blox',
   syntax='proto3',
   serialized_options=None,
-  serialized_pb=_b('\n\x18SetpointSensorPair.proto\x12\x04\x62lox\x1a\x0e\x62rewblox.proto\x1a\x0cnanopb.proto\"\xd8\x01\n\x12SetpointSensorPair\x12\x1d\n\x08sensorId\x18\x02 \x01(\rB\x0b\x8a\xb5\x18\x02\x18\x02\x92?\x02\x38\x10\x12)\n\x07setting\x18\x05 \x01(\x11\x42\x18\x8a\xb5\x18\x02\x30\x01\x8a\xb5\x18\x02\x08\x01\x8a\xb5\x18\x03\x10\x80 \x92?\x02\x38 \x12-\n\x05value\x18\x06 \x01(\x11\x42\x1e\x8a\xb5\x18\x02\x30\x01\x8a\xb5\x18\x02\x08\x01\x8a\xb5\x18\x03\x10\x80 \x92?\x02\x38 \x8a\xb5\x18\x02(\x01\x12\x16\n\x0esettingEnabled\x18\x07 \x01(\x08\x12(\n\x0estrippedFields\x18\x63 \x03(\rB\x10\x8a\xb5\x18\x02(\x01\x92?\x02\x38\x10\x92?\x02\x10\x02:\x07\x8a\xb5\x18\x03\x18\xaf\x02\x62\x06proto3')
+  serialized_pb=_b('\n\x18SetpointSensorPair.proto\x12\x04\x62lox\x1a\x0e\x62rewblox.proto\x1a\x0cnanopb.proto\"\x8f\x02\n\x12SetpointSensorPair\x12\x1d\n\x08sensorId\x18\x02 \x01(\rB\x0b\x8a\xb5\x18\x02\x18\x02\x92?\x02\x38\x10\x12/\n\x07setting\x18\x05 \x01(\x11\x42\x1e\x8a\xb5\x18\x02\x30\x01\x8a\xb5\x18\x02\x08\x01\x8a\xb5\x18\x03\x10\x80 \x92?\x02\x38 \x8a\xb5\x18\x02(\x01\x12-\n\x05value\x18\x06 \x01(\x11\x42\x1e\x8a\xb5\x18\x02\x30\x01\x8a\xb5\x18\x02\x08\x01\x8a\xb5\x18\x03\x10\x80 \x92?\x02\x38 \x8a\xb5\x18\x02(\x01\x12\x16\n\x0esettingEnabled\x18\x07 \x01(\x08\x12/\n\rstoredSetting\x18\x08 \x01(\x11\x42\x18\x8a\xb5\x18\x02\x30\x00\x8a\xb5\x18\x02\x08\x01\x8a\xb5\x18\x03\x10\x80 \x92?\x02\x38 \x12(\n\x0estrippedFields\x18\x63 \x03(\rB\x10\x8a\xb5\x18\x02(\x01\x92?\x02\x38\x10\x92?\x02\x10\x02:\x07\x8a\xb5\x18\x03\x18\xaf\x02\x62\x06proto3')
   ,
   dependencies=[brewblox__pb2.DESCRIPTOR,nanopb__pb2.DESCRIPTOR,])
 
@@ -48,7 +48,7 @@ _SETPOINTSENSORPAIR = _descriptor.Descriptor(
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
-      serialized_options=_b('\212\265\030\0020\001\212\265\030\002\010\001\212\265\030\003\020\200 \222?\0028 '), file=DESCRIPTOR),
+      serialized_options=_b('\212\265\030\0020\001\212\265\030\002\010\001\212\265\030\003\020\200 \222?\0028 \212\265\030\002(\001'), file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
       name='value', full_name='blox.SetpointSensorPair.value', index=2,
       number=6, type=17, cpp_type=1, label=1,
@@ -64,7 +64,14 @@ _SETPOINTSENSORPAIR = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='strippedFields', full_name='blox.SetpointSensorPair.strippedFields', index=4,
+      name='storedSetting', full_name='blox.SetpointSensorPair.storedSetting', index=4,
+      number=8, type=17, cpp_type=1, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=_b('\212\265\030\0020\000\212\265\030\002\010\001\212\265\030\003\020\200 \222?\0028 '), file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='strippedFields', full_name='blox.SetpointSensorPair.strippedFields', index=5,
       number=99, type=13, cpp_type=3, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
@@ -83,7 +90,7 @@ _SETPOINTSENSORPAIR = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=65,
-  serialized_end=281,
+  serialized_end=336,
 )
 
 DESCRIPTOR.message_types_by_name['SetpointSensorPair'] = _SETPOINTSENSORPAIR
@@ -100,6 +107,7 @@ _sym_db.RegisterMessage(SetpointSensorPair)
 _SETPOINTSENSORPAIR.fields_by_name['sensorId']._options = None
 _SETPOINTSENSORPAIR.fields_by_name['setting']._options = None
 _SETPOINTSENSORPAIR.fields_by_name['value']._options = None
+_SETPOINTSENSORPAIR.fields_by_name['storedSetting']._options = None
 _SETPOINTSENSORPAIR.fields_by_name['strippedFields']._options = None
 _SETPOINTSENSORPAIR._options = None
 # @@protoc_insertion_point(module_scope)
