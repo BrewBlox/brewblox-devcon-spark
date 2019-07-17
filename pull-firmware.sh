af96dd6406875054d3c9ccde465e472501b239a0
Bob Steers <steers.bob@gmail.com>
2019-07-17 13:27:02 +0200

    pull system files in pull-firmware

--- pull-firmware.sh
+++ pull-firmware.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
+set -e
 
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-pushd "${SCRIPT_DIR}" > /dev/null
-
+# Args
 TAG=${1:-"develop"}
 echo "Using brewblox/firmware-bin:${TAG}"
+
+# Nagivate to script dir
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+pushd "${SCRIPT_DIR}" > /dev/null
 
 # Pull image
 docker rm bin-box 2> /dev/null || true
@@ -32,6 +35,18 @@ pushd brewblox_devcon_spark/codec > /dev/null
 rm -f ./proto-compiled/*_pb2.py
 protoc -I=./proto --python_out=./proto-compiled ./proto/*.proto
 popd > /dev/null
+
+# Get bootloader and system files
+PARTICLE_VERSION=1.2.1-rc.2
+PARTICLE_RELEASES=https://github.com/particle-iot/firmware/releases/download/v${PARTICLE_VERSION}
+
+curl -sL -o ./binaries/bootloader-p1.bin ${PARTICLE_RELEASES}/p1-bootloader@${PARTICLE_VERSION}.bin
+curl -sL -o ./binaries/system-part1-p1.bin ${PARTICLE_RELEASES}/p1-system-part1@${PARTICLE_VERSION}.bin
+curl -sL -o ./binaries/system-part2-p1.bin ${PARTICLE_RELEASES}/p1-system-part2@${PARTICLE_VERSION}.bin
+
+curl -sL -o ./binaries/bootloader-photon.bin ${PARTICLE_RELEASES}/photon-bootloader@${PARTICLE_VERSION}.bin
+curl -sL -o ./binaries/system-part1-photon.bin ${PARTICLE_RELEASES}/photon-system-part1@${PARTICLE_VERSION}.bin
+curl -sL -o ./binaries/system-part2-photon.bin ${PARTICLE_RELEASES}/photon-system-part2@${PARTICLE_VERSION}.bin
 
 # Pop script dir
 popd > /dev/null
