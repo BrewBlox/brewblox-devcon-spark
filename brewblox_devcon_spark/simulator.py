aa1cd0e7302f370b8d283bd00fe6a9d46c8ebd76
Bob Steers <steers.bob@gmail.com>
2020-03-25 18:13:43 +0100

    avoid coverage errors in simulator

--- brewblox_devcon_spark/simulator.py
+++ brewblox_devcon_spark/simulator.py
@@ -6,9 +6,13 @@ Launches the firmware simulator in a subprocess
 import os
 import subprocess
 
+from brewblox_service import brewblox_logger
 
-def start():
-    print(os.getcwd())
+LOGGER = brewblox_logger(__name__)
+
+
+def start():  # pragma: no cover
+    LOGGER.info(os.getcwd())
     subprocess.Popen(
         ['./brewblox-amd', '--device_id=123456789012345678901234'],
         cwd=f'{os.getcwd()}/binaries')
