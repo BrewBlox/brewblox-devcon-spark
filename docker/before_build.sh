e0887883841844f3fd887c81bbcc42110fc9c458
Bob Steers <steers.bob@gmail.com>
2020-07-03 14:38:42 +0200

    bring before_build in line with others

--- docker/before_build.sh
+++ docker/before_build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # Automatically executed by CI
 set -e
-
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-pushd "$SCRIPT_DIR/.." > /dev/null
+pushd "$(dirname "$(readlink -f "$0")")/.." > /dev/null
 
 rm -rf dist docker/dist docker/binaries
 
@@ -13,5 +11,3 @@ poetry export --without-hashes -f requirements.txt -o docker/requirements.txt
 
 cp -rf dist/ docker/
 cp -rf binaries/ docker/
-
-popd > /dev/null
