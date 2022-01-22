68938fe221fafc400206cee9a6f5f4461ee0bf0d
Bob Steers <steers.bob@gmail.com>
2022-01-22 15:24:52 +0100

    add documentation for firmware pull scripts

--- dev/compile-proto.sh
+++ dev/compile-proto.sh
@@ -5,7 +5,9 @@
 # When pulling external updates, use update-firmware.sh
 #
 set -euo pipefail
-pushd "$(dirname "$0")/../brewblox_devcon_spark/codec" > /dev/null
+pushd "$(dirname "$0")/.." > /dev/null
+. .venv/bin/activate
 
+pushd "brewblox_devcon_spark/codec" > /dev/null
 rm -f ./proto-compiled/*_pb2.py
-protoc -I=./proto --python_out=./proto-compiled ./proto/**.proto
+python3 -m grpc_tools.protoc -I=./proto --python_out=./proto-compiled ./proto/**.proto
