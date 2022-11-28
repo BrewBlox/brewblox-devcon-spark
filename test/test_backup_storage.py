7eace640479e95e571a90e36cf5521f53186a1a6
Bob Steers <steers.bob@gmail.com>
2022-11-28 17:49:50 +0100

    add automated backup storage

---
+++ test/test_backup_storage.py
@@ -0,0 +1,86 @@
+"""
+Tests brewblox_devcon_spark.broadcaster
+"""
+
+from unittest.mock import AsyncMock
+
+import pytest
+from brewblox_service import repeater, scheduler
+
+from brewblox_devcon_spark import (backup_storage, block_store, codec,
+                                   commander, connection_sim, controller,
+                                   global_store, service_status, service_store,
+                                   synchronization)
+from brewblox_devcon_spark.models import Backup, BackupIdentity
+
+TESTED = backup_storage.__name__
+
+
+@pytest.fixture(autouse=True)
+def m_backup_dir(mocker, tmp_path):
+    mocker.patch(TESTED + '.BASE_BACKUP_DIR', tmp_path)
+
+
+@pytest.fixture
+def app(app):
+    app['config']['backup_interval'] = 0.01
+    app['config']['backup_retry_interval'] = 0.01
+    service_status.setup(app)
+    scheduler.setup(app)
+    codec.setup(app)
+    block_store.setup(app)
+    global_store.setup(app)
+    service_store.setup(app)
+    connection_sim.setup(app)
+    commander.setup(app)
+    synchronization.setup(app)
+    controller.setup(app)
+    backup_storage.setup(app)
+
+    backup_storage.fget(app)._autostart = False
+    return app
+
+
+@pytest.fixture
+async def synchronized(app, client):
+    await service_status.wait_synchronized(app)
+
+
+async def test_inactive(app, client, synchronized):
+    app['config']['backup_interval'] = -2
+    app['config']['backup_retry_interval'] = -1
+
+    storage = backup_storage.BackupStorage(app)
+    with pytest.raises(repeater.RepeaterCancelled):
+        await storage.prepare()
+
+    assert storage.retry_interval_s == -2
+    assert not storage.enabled
+    assert not storage.active
+
+
+async def test_autosave(app, client, mocker, synchronized):
+    storage = backup_storage.fget(app)
+    await storage.prepare()
+    await storage.run()
+
+    stored = await storage.all()
+    assert len(stored) == 1
+    assert isinstance(stored[0], BackupIdentity)
+
+    data = await storage.read(stored[0])
+    assert isinstance(data, Backup)
+
+    mocker.patch.object(controller.fget(app), 'make_backup', AsyncMock(side_effect=RuntimeError))
+    with pytest.raises(RuntimeError):
+        await storage.run()
+
+    # Synchronized is checked before controller call
+    # run() exits before the RuntimeError is raised
+    service_status.set_disconnected(app)
+    await service_status.wait_disconnected(app)
+    await storage.run()
+
+    # No new entries were added
+    stored = await storage.all()
+    assert len(stored) == 1
