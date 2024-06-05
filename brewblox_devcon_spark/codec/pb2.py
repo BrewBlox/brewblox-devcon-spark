e55d1b8545d189cde1179362fd9f79b840958411
Bob Steers <steers.bob@gmail.com>
2024-06-05 11:39:43 +0200

    add analog blocks to proto lookups

--- brewblox_devcon_spark/codec/pb2.py
+++ brewblox_devcon_spark/codec/pb2.py
@@ -15,6 +15,7 @@ if 'brewblox_pb2' not in sys.modules:  # pragma: no cover
     import ActuatorLogic_pb2
     import ActuatorOffset_pb2
     import ActuatorPwm_pb2
+    import AnalogGpioModule_pb2
     import Balancer_pb2
     import brewblox_pb2
     import command_pb2
@@ -38,6 +39,7 @@ if 'brewblox_pb2' not in sys.modules:  # pragma: no cover
     import Spark2Pins_pb2
     import Spark3Pins_pb2
     import SysInfo_pb2
+    import TempSensorAnalog_pb2
     import TempSensorCombi_pb2
     import TempSensorExternal_pb2
     import TempSensorMock_pb2
@@ -50,6 +52,7 @@ __all__ = [
     'ActuatorLogic_pb2',
     'ActuatorOffset_pb2',
     'ActuatorPwm_pb2',
+    'AnalogGpioModule_pb2',
     'Balancer_pb2',
     'brewblox_pb2',
     'command_pb2',
@@ -73,6 +76,7 @@ __all__ = [
     'Spark2Pins_pb2',
     'Spark3Pins_pb2',
     'SysInfo_pb2',
+    'TempSensorAnalog_pb2',
     'TempSensorCombi_pb2',
     'TempSensorExternal_pb2',
     'TempSensorMock_pb2',
