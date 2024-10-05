e2e78cba07d303f5b435bba6393b2e65e475cf67
Elco Jacobs <elco@brewpi.com>
2024-10-05 21:23:01 +0200

    fix for enabled setting not being used because settings were changed locally and mqtt change event would compare as equal

--- brewblox_devcon_spark/endpoints/http_settings.py
+++ brewblox_devcon_spark/endpoints/http_settings.py
@@ -8,6 +8,7 @@ from fastapi import APIRouter
 
 from .. import datastore_settings
 from ..models import AutoconnectSettings
+from .. import state_machine
 
 LOGGER = logging.getLogger(__name__)
 
@@ -29,6 +30,9 @@ async def settings_enabled_put(args: AutoconnectSettings) -> AutoconnectSettings
     Set enabled flag.
     """
     store = datastore_settings.CV.get()
-    store.service_settings.enabled = args.enabled
-    await store.commit_service_settings()
+    store.service_settings.enabled = args.enabled # update local settings
+    state_machine.CV.get().set_enabled(args.enabled) # apply settings
+    await store.commit_service_settings() # commit local settings to datastore
+    # change event from MQTT will come in, but settings are the same and callbacks will be skipped.
+    # if we would only apply on MQTT event, toggling enabled would depend on round trip through the datastore and eventbus.
     return AutoconnectSettings(enabled=args.enabled)
