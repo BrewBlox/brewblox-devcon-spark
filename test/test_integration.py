2355578c27c93e0f60bb3d1a40635ef542ed39c7
Elco Jacobs <elco@brewpi.com>
2024-12-05 01:22:32 +0100

    formatting

--- test/test_integration.py
+++ test/test_integration.py
@@ -866,6 +866,7 @@ async def test_debug_encode_response(client: AsyncClient):
     assert payload.content['value']['value'] == 0  # Readonly value
     assert payload.content['offset']['value'] == 20
 
+
 async def test_get_free_port():
     # test for coverage, because tests get their free port from pytest_asyncio
     port = utils.get_free_port()
